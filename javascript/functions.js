079c35f25ae064a4700fa925d218df429d0bbf7f
Robert Crist <rcrist3@uoregon.edu>
2015-04-15 14:08:39 -0700

    Add Images and Stylesheets

---
+++ javascript/functions.js
@@ -0,0 +1,14 @@
+var isArmstrong = function(n){
+	"use strict"; var sum = 0
+	n = String(n);
+for(var i = 0;
+ 	i < n.length;
+ 	++i);
+ 	var pwr=Math.pow();
+sum = sum + pwr
+if(sum == n){
+	return true
+	} else {
+		return false
+};
+};
